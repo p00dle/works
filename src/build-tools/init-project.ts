430ed326b0929e420bc5695cf7b9a18c92ea5079
p00dle <krzysztof.siuda@concentrix.com>
2022-02-21 16:25:58 +0000

    ./works/table-cache.json no longer ignored

--- src/build-tools/init-project.ts
+++ src/build-tools/init-project.ts
@@ -9,6 +9,8 @@ export async function initProject(relativePath: string = '.', {force}: {force?: 
     throw new UserError('Project already initiated. Use -f or --force to overwrite project');
   }
   await dir.copy(sourceDir, destinationDir);
+  const gitIgnoreFile = path.join(__dirname, '../../assets/gitignore-template.txt');
+  await file.copy(gitIgnoreFile, path.join(destinationDir, '.gitignore'));
   return 'Project initiated successfully';
 }
 
