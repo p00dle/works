08a035c2685b9231557442cfccee5f91a5412a90
p00dle <krzysztof.siuda@concentrix.com>
2022-02-21 16:28:13 +0000

    string -> text

--- assets/project-template/components/user-telemetry-log/table.config.ts
+++ assets/project-template/components/user-telemetry-log/table.config.ts
@@ -5,10 +5,10 @@ export const table: Table =  {
   name: 'userTelemetryLogs',
   columns: [
     { name: 'uuid', type: 'uuid', unique: true },
-    { name: 'type', type: 'string', nullable: false },
+    { name: 'type', type: 'text', nullable: false },
     { name: 'username', type: 'text', nullable: false },
-    { name: 'path', type: 'string', nullable: false },
-    { name: 'timestamp', type: 'timestamp', nullable: false },
+    { name: 'path', type: 'text', nullable: false },
+    { name: 'timestamp', type: 'datetime', nullable: false },
     { name: 'interval', type: 'integer', nullable: true },
     { name: 'details', type: 'jsonb', nullable: true }
   ],
