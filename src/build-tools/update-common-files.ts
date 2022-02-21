101bc29697cf31f0bd9241ccc77d340b80d3cf92
p00dle <krzysztof.siuda@concentrix.com>
2022-02-21 16:30:35 +0000

    reformatted and fixed regex

--- src/build-tools/update-common-files.ts
+++ src/build-tools/update-common-files.ts
@@ -4,144 +4,93 @@ import { getNames, Names } from '../lib/get-names';
 import { getRootDir } from '../lib/get-rootdir';
 import { Table } from '../types/table';
 
+type WorksDirective =
+  | 'next_import'
+  | 'next_value'
+  | 'next_get_api'
+  | 'next_post_api'
+  | 'next_type_import'
+  | 'next_get_endpoints'
+  | 'next_post_endpoints'
+  | 'next_route'
+  | 'next_types'
+;
+
+type Filepath = string;
+type WorksDirectiveReplacer = (componentNames: Names, componentPath: string) => string;
+
+const commonFilesMetadata: Record<Filepath, { next_import: WorksDirectiveReplacer } & Partial<Record<WorksDirective, WorksDirectiveReplacer>>> = {
+  'types/models.ts': {
+    next_import: (names, componentPath) => `export type { ${names.single.pascal} } from '~/components${componentPath}/types';`,
+  },
+  'types/events.ts': {
+    next_import: (names, componentPath) => `import type { ${names.single.pascal}Events } from '~/components${componentPath}/events';`,
+    next_value: names => `  & ${names.single.pascal}Events`,
+  },
+  'types/http-api.ts': {
+    next_import: (names, componentPath) => `import type { ${names.single.pascal}GetEndpoints, ${names.single.pascal}PostEndpoints } from '~/components${componentPath}/http-endpoints';`,
+    next_get_api: names => `  & ${names.single.pascal}GetEndpoints`,
+    next_post_api: names => `  & ${names.single.pascal}PostEndpoints`,
+  },
+  'types/csv-api.ts': {
+    next_import: (names, componentPath) => `import type { ${names.single.pascal}CsvApi } from '~/components${componentPath}/csv-endpoints';`,
+    next_value: names => `  & ${names.single.pascal}CsvApi`
+  },
+  'types/ws-api.ts': {
+    next_import: (names, componentPath) => `import type { ${names.single.pascal}WsApi } from '~/components${componentPath}/ws-channels';`,
+    next_value: names => `  & ${names.single.pascal}WsApi`
+  },
+  'types/key-values.ts': {
+    next_import: (names, componentPath) => `import { initial${names.single.pascal}KeyValues} from '~/components${componentPath}/key-values';`,
+    next_type_import: (names, componentPath) => `import type { ${names.single.pascal}KeyValues } from '~/components${componentPath}/key-values';`,
+    next_types: names => `  & ${names.single.pascal}KeyValues`,
+    next_value: names => `  initial${names.single.pascal}KeyValues,`,
+  },
+  'bootstrap/api-router.ts': {
+    next_import: (names, componentPath) => `import { ${names.single.camel}GetEndpoints, ${names.single.camel}PostEndpoints } from '~/components${componentPath}/http-endpoints';`,
+    next_get_endpoints: names => `  ${names.single.camel}GetEndpoints`,
+    next_post_endpoints: names => `  ${names.single.camel}PostEndpoints`,
+  },
+  'bootstrap/ws-pubsub.ts': {
+    next_import: (names, componentPath) => `import { ${names.single.camel}WsChannels } from '~/components${componentPath}/http-endpoints';`,
+    next_route: names => `  ${names.single.camel}WsChannels,`,
+  }
+}
+
+
+const regexCache: Record<string, RegExp> = {}
 function replaceWorksDirectives(text: string, directives: Record<string, string>): string {
   let output = text;
   for (const [directive, replaceWith] of Object.entries(directives)) {
-    output = output.replace(new RegExp(`\\s*//\\s*works:${directive}`), match => {
+    if (!regexCache[directive]) regexCache[directive] = new RegExp(`[\\t ]*//[\\t ]*@works:${directive}`);
+    console.log(directive, regexCache[directive], regexCache[directive].test(output));
+
+    output = output.replace(regexCache[directive], match => {
       return replaceWith + '\n' + match;
     });
   }
   return output;
 }
 
-async function updateModels(componentRelativePath: string, rootDir: string, names: Names) {
-  const targetFile = path.join(rootDir, 'types', 'models.ts');
-  const initialText = await file.read.text(targetFile);
-  // TODO: assuming that the component will always export its own name; this needs to be done with some file introspection ( maybe tsc, maybe just file read)
-  if (!initialText.includes(names.single.pascal)) {
-    const updatedText = replaceWorksDirectives(initialText, {
-      'next_import': `export type { ${names.single.pascal} } from '~/components${componentRelativePath}/types';`,
-    })
-    await file.write.text(targetFile, updatedText);
-  }
-}
-
-async function updateEvents(componentRelativePath: string, rootDir: string, names: Names) {
-  const name = names.single.pascal;
-  const targetFile = path.join(rootDir, 'types', 'events.ts');
-  const initialText = await file.read.text(targetFile);
-  const importStatement = `import type { ${name}Events } from '~/components${componentRelativePath}/events';`;
-  if (!initialText.includes(importStatement)) {
-    const updatedText = replaceWorksDirectives(initialText, {
-      'next_import': importStatement,
-      'next_value': `  & ${name}Events`
-    })
-    await file.write.text(targetFile, updatedText);
-  }
-}
-
-async function updateHttpApi(componentRelativePath: string, rootDir: string, names: Names) {
-  const name = names.single.pascal;
-  const targetFile = path.join(rootDir, 'types', 'http-api.ts');
-  const initialText = await file.read.text(targetFile);
-  const importStatement = `import type { ${name}GetEndpoints, ${name}PostEndpoints } from '~/components${componentRelativePath}/http-endpoints';`;
-  if (!initialText.includes(importStatement)) {
-    const updatedText = replaceWorksDirectives(initialText, {
-      'next_import': importStatement, 
-      'next_get_api': `  ${name}GetEndpoints`,
-      'next_post_api': `  ${name}PostEndpoints`,
-    })
-    await file.write.text(targetFile, updatedText);
-  }
-}
-
-async function updateCsvApi(componentRelativePath: string, rootDir: string, names: Names) {
-  const name = names.single.pascal;
-  const targetFile = path.join(rootDir, 'types', 'csv-api.ts');
-  const initialText = await file.read.text(targetFile);
-  const importStatement = `import type { ${name}CsvApi } from '~/components${componentRelativePath}/csv-endpoints';`;
-  if (!initialText.includes(importStatement)) {
-    const updatedText = replaceWorksDirectives(initialText, {
-      'next_import': importStatement,
-      'next_value': `  & ${name}CsvApi`
-    })
-    await file.write.text(targetFile, updatedText);
-  }
-}
-
-async function updateWsApi(componentRelativePath: string, rootDir: string, names: Names) {
-  const name = names.single.pascal;
-  const targetFile = path.join(rootDir, 'types', 'ws-api.ts');
-  const initialText = await file.read.text(targetFile);
-  const importStatement = `import type { ${name}WsApi } from '~/components${componentRelativePath}/ws-channels';`;
-  if (!initialText.includes(importStatement)) {
-    const updatedText = replaceWorksDirectives(initialText, {
-      'next_import': importStatement,
-      'next_value': `  & ${name}WsApi`
-    })
-    await file.write.text(targetFile, updatedText);
-  }
-}
-
-async function updateKeyValues(componentRelativePath: string, rootDir: string, names: Names) {
-  const name = names.single.pascal;
-  const targetFile = path.join(rootDir, 'types', 'key-values.ts');
-  const initialText = await file.read.text(targetFile);
-  const typeImportStatement = `import type { ${name}KeyValues } from '~/components${componentRelativePath}/key-values';`;
-  // const typeImportStatement = `import type { ${name}KeyValues, initial${name}KeyValues } from '~/components${componentRelativePath}/key-values';\n`;
-  if (!initialText.includes(typeImportStatement)) {
-    const updatedText = replaceWorksDirectives(initialText, {
-      'next_type_import': typeImportStatement,
-      'next_value_import': `import { initial${name}KeyValues} from ~/components${componentRelativePath}/key-values';`,
-      'next_type': `  & ${name}KeyValues`,
-      'next_value': `  & initial${name}KeyValues`,
-    })
-    await file.write.text(targetFile, updatedText);
-  }
-}
-
-async function updateApiRouter(componentRelativePath: string, rootDir: string, names: Names) {
-  const name = names.single.camel;
-  const targetFile = path.join(rootDir, 'bootstrap', 'api-router.ts');
-  const initialText = await file.read.text(targetFile);
-  const importStatement = `import { ${name}GetEndpoints, ${name}PostEndpoints } from '~/components${componentRelativePath}/http-endpoints';\n`;
-  if (!initialText.includes(importStatement)) {
-    const updatedText = replaceWorksDirectives(initialText, {
-      'next_import': importStatement,
-      'next_get_endpoints': `  ${name}GetEndpoints`,
-      'next_post_endpoints': `  ${name}PostEndpoints`,
-    });
-    await file.write.text(targetFile, updatedText);
-  }
-}
-
-async function updateWsPubSub(componentRelativePath: string, rootDir: string, names: Names) {
-  const name = names.single.camel;
-  const targetFile = path.join(rootDir, 'bootstrap', 'ws-pubsub.ts');
-  const initialText = await file.read.text(targetFile);
-  const importStatement = `import { ${name}WsApi } from '~/components${componentRelativePath}/http-endpoints';\n`;
-  if (!initialText.includes(importStatement)) {
-    const updatedText = replaceWorksDirectives(initialText, {
-      'next_import': importStatement,
-      'next_route': `  ${name}WsApi`,
-    });
-    await file.write.text(targetFile, updatedText);
-  }
-}
-
-
-// TODO: files need to be read to determine whether they have exports and not update common files when that's the case
-export async function updateCommonFiles(componentRelativePath: string, table: Table) {
+// TODO: account for different naming conventions
+export async function updateCommonFiles(componentPath: string, table: Table) {
   const rootDir = getRootDir();
-  const names = getNames(path.basename(componentRelativePath));
-  await Promise.all([
-    updateModels,
-    updateEvents,
-    updateHttpApi,
-    updateCsvApi,
-    updateKeyValues,
-    updateWsApi,
-    updateApiRouter,
-    updateWsPubSub
-  ].map(fn => fn(componentRelativePath, rootDir, names)));
+  const names = getNames(path.basename(componentPath));
+  await Promise.all(Object.entries(commonFilesMetadata).map(async ([relativeTargetPath, directiveReplacers]) => {
+    const targetFile = path.join(rootDir, relativeTargetPath);
+    const fileText = await file.read.text(targetFile);
+    const directives: {next_import: string} & Record<string, string> = { next_import: ''};
+    for (const [directive, directiveReplacer] of Object.entries(directiveReplacers)) {
+      directives[directive] = directiveReplacer(names, componentPath);
+    }
+    console.log('current file: ');
+    console.log(fileText);
+    console.log('next_import: ' + directives.next_import);
+    console.log('!fileText.includes(directives.next_import): ' + !fileText.includes(directives.next_import));
+    if (!fileText.includes(directives.next_import)) {
+      const updatedFileText = replaceWorksDirectives(fileText, directives);
+      console.log('\n\n\n------------');
+      await file.write.text(targetFile, updatedFileText);
+    }
+  }));
 }
